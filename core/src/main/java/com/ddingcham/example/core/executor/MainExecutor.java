f194dc0bc1d591bf8abc9ab7e0e5ec7dab6a52f6
ddingcham <qo0op308@gmail.com>
2019-03-09 15:20:50 +0900

    Add Simple Executor Example

--- core/src/main/java/com/ddingcham/example/core/executor/MainExecutor.java
+++ core/src/main/java/com/ddingcham/example/core/executor/MainExecutor.java
@@ -1,18 +1,30 @@
 package com.ddingcham.example.core.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class MainExecutor implements Executor {
 
+    private BlockingQueue<Runnable> tasks = new LinkedBlockingDeque<>();
+
     public void runLoop() throws InterruptedException {
         while(!Thread.interrupted()) {
-            // task run
+            tasks.take().run();
         }
     }
 
     @Override
     public void execute(Runnable command) {
         // tasks : add command
-        System.out.println(command);
+        System.out.println("add task :  " + command);
+        tasks.add(command);
+    }
+
+    public void shutdown() {
+        Thread currentThread = Thread.currentThread();
+        if(currentThread.isAlive() && !currentThread.isInterrupted()) {
+            currentThread.interrupt();
+        }
     }
 }
