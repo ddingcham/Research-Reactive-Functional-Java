1f5196812d48843788de5bc21b7bd0ec4119a92c
ddingcham <qo0op308@gmail.com>
2019-03-12 15:27:59 +0900

    fix VideoRepositoryTest simple launch

--- core/src/test/java/com/ddingcham/example/core/service/VideoRepositoryTest.java
+++ core/src/test/java/com/ddingcham/example/core/service/VideoRepositoryTest.java
@@ -1,27 +1,41 @@
 package com.ddingcham.example.core.service;
 
+import com.ddingcham.example.core.util.Logger;
 import org.junit.Test;
 
-import static com.ddingcham.example.core.util.Logger.log;
 import static com.ddingcham.example.core.util.ThreadUtil.sleep;
 
 public class VideoRepositoryTest {
 
-    VideoRepository repository = new VideoRepository();
+    private VideoRepository repository = new VideoRepository();
 
     @Test
     public void getVideos_simple_launch() {
         repository
                 .getVideos()
-                .forEach(video -> log(video));
+                .subscribe(Logger::log);
         sleep(2500);
+    }
+
+    @Test
+    public void getVideos_blocking_launch() {
+        repository
+                .getVideos()
+                .blockingForEach(Logger::log);
     }
 
     @Test
     public void getVideosWithErrors_simple_launch() {
         repository
                 .getVideosWithErrors()
-                .doOnError(error -> log(error.getMessage()));
+                .subscribe(Logger::log, Logger::showThread);
          sleep(2500);
+    }
+
+    @Test
+    public void getVideosWithErrors_blocking_launch() {
+        repository
+                .getVideosWithErrors()
+                .blockingSubscribe(Logger::log, Logger::showThread);
     }
 }
